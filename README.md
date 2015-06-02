dcc00d5d2759a399c9819dc18d408c1d9242ab0d
André Poleza <andrepoleza@nepente.io>
2015-06-02 00:09:22 -0300

    Adds more info about the module

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # church-encoding [![Build Status](https://travis-ci.org/andrepoleza/church-encoding.svg?branch=master)](https://travis-ci.org/andrepoleza/church-encoding)
-Church Encoding in JavaScript
+
+> Church Encoding in JavaScript
+
+## Install
+
+```
+$ npm install --save church-encoding
+```
+
+
+## Usage
+
+```js
+var churchEncoding = require('church-encoding');
+
+//=> Exposes "zero", "one", "two" and "three" as functions (for now)
+```
--- index.js
+++ index.js
@@ -3,31 +3,31 @@
 // Applies "f" to "x" zero times.
 // λf.λx.x
 module.exports.zero = function(f) {
-    return function(x) {
-        return x;
-    };
+	return function(x) {
+		return x;
+	};
 };
 
 // Applies the fn "f" to the arg "x" once.
 // λf.λx.f x
 module.exports.one = function(f) {
-    return function(x) {
-        return f(x);
-    };
+	return function(x) {
+		return f(x);
+	};
 };
 
 // Applies the fn "f" to the arg "x" twice.
 // λf.λx.f (f x)
 module.exports.two = function(f) {
-    return function(x) {
-        return f(f(x));
-    };
+	return function(x) {
+		return f(f(x));
+	};
 };
 
 // Applies the fn "f" to the arg "x" three times.
 // λf.λx.f (f (f x))
 module.exports.three = function(f) {
-    return function(x) {
-        return f(f(f(x)));
-    };
+	return function(x) {
+		return f(f(f(x)));
+	};
 };
