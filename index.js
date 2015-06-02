fd91a99ae0e11f6477aaa30fbdae7d531ce8f186
André Poleza <andrepoleza@nepente.io>
2015-06-02 02:20:08 -0300

    Splits each impl in one single file

--- index.js
+++ index.js
@@ -2,63 +2,12 @@
 
 // NOTICE: ALL FUNCTIONS >>>MUST BE<<< CURRIED!
 
-// Converts the Church number "n" (a function) to its correspondent integer.
-function churchToInteger(n) {
-	return n(function(x) { return x + 1; })(0);
+module.exports = {
+	zero: require('./lib/zero'),
+	one: require('./lib/one'),
+	two: require('./lib/two'),
+	three: require('./lib/three'),
+	successor: require('./lib/successor'),
+	addition: require('./lib/addition'),
+	churchToInteger: require('./lib/churchToInteger')
 }
-
-// λf.λx.x
-function zero(f) {
-	return function(x) {
-		return x;
-	}
-}
-
-// λf.λx.f x
-function one(f) {
-	return function(x) {
-		return f(x);
-	}
-}
-
-// λf.λx.f (f x)
-function two(f) {
-	return function(x) {
-		return f(f(x));
-	}
-}
-
-// λf.λx.f (f (f x))
-function three(f) {
-	return function(x) {
-		return f(f(f(x)));
-	}
-}
-
-// λn.λf.λx.f (n f x)
-function successor(n) {
-	return function(f) {
-		return function(x) {
-			return f(n(f)(x));
-		}
-	}
-}
-
-// λa.λb.f.λx.a f (n f x)
-function addition(a) {
-	return function(b) {
-		return function(f) {
-			return function(x) {
-				return a(f)(b(f)(x));
-			}
-		}
-	}
-}
-
-module.exports.zero = zero;
-module.exports.one = one;
-module.exports.two = two;
-module.exports.three = three;
-module.exports.successor = successor;
-module.exports.addition = addition;
-module.exports.churchToInteger = churchToInteger;
